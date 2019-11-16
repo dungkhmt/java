74bb1030e518f1fd6b08f1abb4086af4088dad7c
Duong Trung Hieu <dthieu.cur.2201@gmail.com>
2019-11-17 03:50:14 +0700

    fix

--- src/studentpractice/DuongTrungHieu/sort.java
+++ src/studentpractice/DuongTrungHieu/sort.java
@@ -24,7 +24,7 @@ public class sort {
 		a[j] = x;
 	}
 	public void print(int low , int high) {
-		for(int i = low ; i < high ; i++) System.out.print(a[i] + " ");
+		for(int i = low ; i <= high ; i++) System.out.print(a[i] + " ");
 		System.out.println();
 	}
 	private int partition(int low ,int high) {
@@ -40,6 +40,20 @@ public class sort {
 		swap(i + 1 , high);
 		return i + 1;
 	}
+	public void bubbleSort(int low ,int high) {
+		int swap;
+		do {
+			swap=0;
+			for(int i=low;i<high-1;i++) {
+				if( a[i] > a[i+1]) {
+					int tmp = a[i];
+					a[i]=a[i+1];
+					a[i+1]=tmp;
+					swap=1;
+				}
+			}
+		}while(swap==1);
+	}
 	public void quickSort(int low , int high) {
 		if(low >= high) return ;
 		int mid = partition(low , high);
@@ -51,7 +65,9 @@ public class sort {
 		sort arr = new sort();
 		arr.inputData("data/data.txt");
 		arr.quickSort(0, arr.a.length - 1);
-		arr.print(0 , arr.a.length );
+		arr.print(0 , arr.a.length -1 );
+		arr.bubbleSort(0, arr.a.length -1);
+		arr.print(0, arr.a.length -1);
 	}
 
 }
