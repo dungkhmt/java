8198de39076014f52f71db94900b6f993bd7eb92
Duong Trung Hieu <dthieu.cur.2201@gmail.com>
2019-11-17 03:33:48 +0700

    first committion

---
+++ src/studentpractice/DuongTrungHieu/sort.java
@@ -0,0 +1,57 @@
+package sort;
+
+import java.io.File;
+import java.util.Scanner;
+public class sort {
+	private int[] a;
+	private int n;
+	public void inputData(String filename) {
+		try {
+			Scanner in = new Scanner(new File(filename));
+			n = in.nextInt();
+			a = new int[n];
+			for(int i=0; i< a.length; i++) 
+				a[i]=in.nextInt();
+			in.close();
+			}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	private void swap(int i , int j) {
+		int x = a[i];
+		a[i] = a[j];
+		a[j] = x;
+	}
+	public void print(int low , int high) {
+		for(int i = low ; i < high ; i++) System.out.print(a[i] + " ");
+		System.out.println();
+	}
+	private int partition(int low ,int high) {
+		int pivot = a[high];
+		int i = low - 1;
+		for(int j = low ; j < high ; j++) {
+			if(a[j] < pivot)
+			{
+				i++;
+				swap(i,j);
+			}
+		}
+		swap(i + 1 , high);
+		return i + 1;
+	}
+	public void quickSort(int low , int high) {
+		if(low >= high) return ;
+		int mid = partition(low , high);
+		quickSort(low , mid - 1);
+		quickSort(mid + 1, high);
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sort arr = new sort();
+		arr.inputData("data/data.txt");
+		arr.quickSort(0, arr.a.length - 1);
+		arr.print(0 , arr.a.length );
+	}
+
+}
