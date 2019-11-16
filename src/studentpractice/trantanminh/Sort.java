c61a826b1783c317fc58fd2ee5b5acdfc0ffae8e
TranTanMinh <tanminhtran168@gmail.com>
2019-11-16 23:05:26 +0700

    Modify again

--- src/studentpractice/trantanminh/Sort.java
+++ src/studentpractice/trantanminh/Sort.java
@@ -114,7 +114,7 @@ public class Sort {
     public static void main(String[] args) {
         Sort app = new Sort();
         app.inputData("data/dayso.txt");
-		app.heapSort(6);
+		app.heapSort(5);
         app.print();
     }
 }
