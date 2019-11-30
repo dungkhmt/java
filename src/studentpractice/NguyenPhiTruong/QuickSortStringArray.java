c708ebdda21c22cd3840375891b9b57384b598a5
Phi Truong Nguyen <phitruong.nd2k@gmal.com>
2019-11-30 09:17:28 +0700

    add BST

---
+++ src/studentpractice/NguyenPhiTruong/QuickSortStringArray.java
@@ -0,0 +1,35 @@
+package studentpractice.NguyenPhiTruong;
+
+public class QuickSortStringArray {
+	public void swap(String[] a, int i, int j) {
+		String tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
+
+	public int partition(String[] a, int L, int R, int indexPivot) {
+		String pivot;
+		pivot = a[indexPivot];
+		int storeIndex = L;
+		swap(a, indexPivot, R);
+		for (int i = L; i < R; i++) {
+			if (a[i].compareTo(pivot) < 0) {
+				swap(a, i, storeIndex);
+				storeIndex++;
+			}
+		}
+		swap(a, storeIndex, R);
+		return storeIndex;
+	}
+
+	public void quickSort(String[] a, int L, int R) {
+		if (L < R) {
+			int index = (L + R) / 2;
+			index = partition(a, L, R, index);
+			if (L < index)
+				quickSort(a, L, index - 1);
+			if (index < R)
+				quickSort(a, index + 1, R);
+		}
+	}
+}
