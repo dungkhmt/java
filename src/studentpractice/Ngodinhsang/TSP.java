d2c31b6140a5de0623bf246e68b2799c4297e2ba
unknown <sanga1k30@gmail.com>
2019-12-01 10:05:00 +0700

    add code

--- src/studentpractice/Ngodinhsang/TSP.java
+++ src/studentpractice/Ngodinhsang/TSP.java
@@ -3,22 +3,22 @@ package studentpractice.Ngodinhsang;
 import java.util.*;
 
 public class TSP {
-	public static int Max=10001;
-	public static int n;
-	public static int f=0;
-	public static int fmin=10000;
-	public static int cmin=10000;
-	public static int a[][]= new int [Max][Max];
-	public static int x[]=new int [Max];
-	public static int xmin[]=new int [Max];
+	public  int Max=10001;
+	public  int n;
+	public  int f=0;
+	public  int fmin=10000;
+	public  int cmin=10000;
+	public  int a[][]= new int [Max][Max];
+	public  int x[]=new int [Max];
+	public  int xmin[]=new int [Max];
 	
-	public static boolean check(int k, int i)
+	public boolean check(int k, int i)
 	{
 		for(int j=2;j<k;j++)
 			if(x[j]==i) return false;
 		return true;
 	}
-	public static void BK(int k)
+	public void BK(int k)
 	{
 		for(int i=2;i<=n;i++)
 		{
