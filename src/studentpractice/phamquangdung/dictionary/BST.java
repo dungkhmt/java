c1fc688e6cca15ab35be581e5e9e6139f92b25a9
dungkhmt <dungkhmt@gmail.com>
2019-12-01 10:07:07 +0700

    add data words.txt

---
+++ src/studentpractice/phamquangdung/dictionary/BST.java
@@ -0,0 +1,26 @@
+package studentpractice.phamquangdung.dictionary;
+
+public class BST {
+	private Node root;
+	
+	public BST(){
+		root = null;
+	}
+	private Node find(Node r, String enWord){
+		if(r == null) return null;
+		int c = r.getEnWord().compareTo(enWord);
+		if(c == 0) return r;
+		if(c < 0) return find(r.getRightChild(),enWord);
+		return find(r.getLeftChild(),enWord);
+	}
+	public String find(String enWord){
+		Node p = find(root, enWord);
+		if(p == null) return null;
+		return p.getVnMeaning();
+	}
+	public void set(String enWord, String vnMeaning){
+		Node p = find(root, enWord);
+		if(p == null) return;
+		p.setVnMeaning(vnMeaning); 
+	}
+}
