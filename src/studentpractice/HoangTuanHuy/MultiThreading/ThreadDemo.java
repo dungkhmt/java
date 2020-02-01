36db26f4bfe8288c2bdb88f17d630a35351a1b94
Hoang Tuan Huy <kubin2712@gmail.com>
2020-02-01 19:46:45 +0700

    Demo

---
+++ src/studentpractice/HoangTuanHuy/MultiThreading/ThreadDemo.java
@@ -0,0 +1,42 @@
+package studentpractice.HoangTuanHuy.MultiThreading;
+
+import java.util.Scanner;
+
+public class ThreadDemo implements Runnable
+{
+	private Thread t;
+	private String ThreadName;
+	private int ID;
+	private String PassWord;
+	private Scanner scan = new Scanner(System.in);
+	ThreadDemo(int id)
+	{
+		this.ID=id;
+	}
+	public void run()
+	{
+		//System.out.println("Running " + ThreadName);
+		try
+		{
+			System.out.println("Username : ");
+			ThreadName = scan.nextLine();
+			System.out.println("PassWord : ");
+			PassWord =  scan.nextLine();
+		}catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		System.out.println("Registered");
+		System.out.println("Your ID : " + ID);
+		t.suspend();
+	}
+	public void start()
+	{
+		System.out.println("Prepare to creat your ID");
+		if(t==null)
+		{
+			t=new Thread(this);
+			t.run();
+		}
+	}
+}
