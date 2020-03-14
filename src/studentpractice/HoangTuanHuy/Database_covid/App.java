016f32d83803ed3aa195f1a98e0a4e60a0a82ec4
Hoang Tuan Huy <kubin2712@gmail.com>
2020-03-15 00:44:28 +0700

    update function

--- src/studentpractice/HoangTuanHuy/Database_covid/App.java
+++ src/studentpractice/HoangTuanHuy/Database_covid/App.java
@@ -7,7 +7,20 @@ import com.sun.net.httpserver.Authenticator.Result;
 
 public class App 
 {
-	public void run() throws Exception
+	private String insert_into_table(String country_id, String country_name,String area_id,int total_cases,int total_deaths)
+	{
+		return ("insert into covid_country(country_id,country_name,area_id,total_cases,total_deaths) values(" +
+				"'" + country_id+ "'," +
+				"'" + country_name+"',"+
+				"'" + area_id + "',"+
+				total_cases+","+
+				total_deaths+")");
+	}
+	private String show_table(String what , String where)
+	{
+		return "select "+what+" from " +where;
+	}
+	private void run() throws Exception
 	{
 		Class.forName("org.postgresql.Driver");
 		
@@ -16,13 +29,17 @@ public class App
 		
 		System.out.println("Successfully Access.");
 		Statement statement =  connect.createStatement(); 
+		//String sql = insert_into_table("BRA","BRAZIL","SA",151,0);
+		//System.out.println(sql);
+		//System.out.println(show_table("*","country_covid"));
+		//statement.executeQuery(sql);
 		ResultSet rs = statement.executeQuery(
-				"SELECT * FROM covid_country"
+				show_table("*","covid_country")
 				);
 		while(rs.next())
 		{
 			System.out.println(rs.getString("country_name") + "\t" + rs.getString("country_id") +
-					"\t" + rs.getString("area_id")+"\t"+rs.getInt("total_cases")+"\t"+rs.getInt("totla_deaths"));
+					"\t" + rs.getString("area_id")+"\t"+rs.getInt("total_cases")+"\t"+rs.getInt("total_deaths"));
 		}
 		rs.close();
 		statement.close();
